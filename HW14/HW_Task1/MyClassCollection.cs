076b5850500a96cdea0aa9a6bd48ae6b1b946b37
Aleksew <aleksewww@gmail.com>
2021-08-03 19:46:11 +0300

    comit

---
+++ HW14/HW_Task1/MyClassCollection.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+
+namespace HW_Task1
+{
+    class MyClassCollection
+    {
+        public string[] strArr = new string[10];
+
+        public IEnumerable GetEvenIndexValues()
+        {
+            string[] strTemp = new string[0];
+
+            var myClassCollection = new MyClassCollection();
+            for (int i = 0; i < strArr.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    yield return strArr[i];
+                }
+            }
+        }
+
+        public IEnumerable GetOddIndexValues()
+        {
+            string[] strTemp = new string[0];
+
+            var myClassCollection = new MyClassCollection();
+            for (int i = 0; i < strArr.Length; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    yield return strArr[i];
+                }
+            }
+        }
+    }
+}
---
+++ HW14/HW_Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace HW_Task1
+{
+    class Program
+    {
+        //Создайте класс MyClassCollection.В теле класса создайте открытый строковой массив на 10 элементов.Создайте методы GetEvenIndexValues() и GetOddIndexValues(), 
+        //возвращающие IEnumerable.С помощью оператора yield реализуйте в телах методов механизм обращения к массиву строк и возврат всех значений, хранящихся по четному и нечетному индексу соответственно.
+        //В методе Main создайте экземпляр класса MyClassCollection, заполните его внутренний массив и в циклах foreach обойдите получаемые сначала из GetEvenIndexValues() и потом из GetOddIndexValues() значения.
+
+        static void Main(string[] args)
+        {
+            var myClassCollection = new MyClassCollection();
+
+            myClassCollection.strArr = new string[] { "Один", "Два", "Три", "Четыре", "Пять", "Шесть", "Семь", "Восемь", "Девять", "Десять" };
+
+            Console.WriteLine("Чётные");
+
+            foreach (var item in myClassCollection.GetEvenIndexValues())
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine(new string('-', 50));
+            Console.WriteLine("Не чётные");
+
+            foreach (var item in myClassCollection.GetOddIndexValues())
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
